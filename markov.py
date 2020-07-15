110609b461a027b8a40928142eb1ffa171399636
Fernanda Diomede <fcdiomede@gmail.com>
2020-07-15 18:40:11 -0500

    Added code to support discord bot

--- markov.py
+++ markov.py
@@ -2,6 +2,8 @@
 
 import sys
 from random import choice
+import os
+import discord
 
 
 def open_and_read_file(filenames):
@@ -64,3 +66,23 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+#adding in discord posting functionality
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'Successfully connected! Logged in as {client.user}.')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send(chains)
+
+
+client.run(os.environ['DISCORD_TOKEN'])
